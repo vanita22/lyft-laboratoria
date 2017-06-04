e68a3496539b351e6618a8490c75ca7a3b4c5ad9
vanita22 <vanitayrafita2004@gmail.com>
2017-06-03 23:07:16 -0300

    actiualizando cambios con javascript

--- js/lyft.js
+++ js/lyft.js
@@ -1,3 +1,5 @@
+/* lo que tenia inicialmente que encontre por internet pero me funcionaba con el link de otra página.....
+
 window.onscroll = function(){
 
 	if(document.body.scrollTop > 90){
@@ -6,8 +8,19 @@ window.onscroll = function(){
 	}
 
 	if(document.body.scrollTop < 90){
-		document.getElementById('menus').style.background = "transparent";
-				
+		document.getElementById('menus').style.background = "transparent";				
 	}
-}
-}
+}*/
+
+var menus = document.getElementById('menus');
+
+window.onscroll = function(){
+	"use strict";
+	if (document.body.scrollTop > 90){
+		menus.classList.add('menu3');
+		menus.classList.remove('menu2');
+	} else{
+		menus.classList.add('menu2');
+		menus.classList.remove('menu3');
+	}
+};
