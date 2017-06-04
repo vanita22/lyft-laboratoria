c6940db52218e364f8cd812665ab53ce23e39ccd
vanita22 <vanitayrafita2004@gmail.com>
2017-06-04 13:42:44 -0300

    subiendo cambios agregando slider

--- js/lyft.js
+++ js/lyft.js
@@ -12,16 +12,43 @@ window.onscroll = function(){
 	}
 }*/
 
-var menus = document.getElementById('menus');
-
 window.onscroll = function(){
+
+	var menus = document.getElementById('menus');
+	var menus2 = document.getElementById('menus2');
 	
 	if (document.body.scrollTop > 130){
-		menus.classList.add('menu3');
-		menus.classList.remove('menu2');
+		menus.classList.add('menus2');
+		menus.classList.remove('menus');
 	} else{
-		menus.classList.add('menu2');
-		menus.classList.remove('menu3');
+		menus.classList.add('menus');
+		menus.classList.remove('menus2');
 	}
-};
+}
+
+
+$(window).load(function(){
+	$('#sliders').nivoSlider({
+		effect: 'random',
+		slices: 15,
+		boxCols: 8,
+		boxRows: 3,
+		animSpeed: 600,
+		pauseTime: 1500,
+		startSlide: 0,
+		directionNav: true,
+		controlNav: true,
+		controlNavThumbs: false,
+		pauseOnHover: true,
+		manualAdvance: false,
+		prevText: '',
+		nextText: '',
+		randomStart: false,
+		beforeChange: function(){},
+		afterChange: function(){},
+		slideshowEnd: function(){},
+		lastSlide: function(){},
+		afterLoad: function(){}
+	});		
+});
 
